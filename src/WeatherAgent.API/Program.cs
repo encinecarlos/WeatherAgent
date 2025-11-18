e56069db2a5b9ea1e8736501f34118fe292f345d
Carlos Encine <carlos.encine@outlook.com>
2025-11-18 18:33:34 -0300

    Refactor Function1 and add telemetry/logging support
    Updated Function1 to only allow HTTP GET requests. Enhanced the Azure Functions application with telemetry and logging capabilities using Application Insights, OpenTelemetry, and Serilog. Added necessary package references to support these features.

--- src/WeatherAgent.API/Program.cs
+++ src/WeatherAgent.API/Program.cs
@@ -2,10 +2,15 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WeatherAgent.API.Extensions;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
+
+builder.Services
+    .ConfigureLogging(builder.Configuration)
+    .AddTelemetry(builder.Configuration);
 
 builder.Services
     .AddApplicationInsightsTelemetryWorkerService()
