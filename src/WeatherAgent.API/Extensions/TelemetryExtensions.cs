a254b82a981c091937b22747bc8067190c64a8dd
Carlos Encine <carlos.encine@outlook.com>
2025-11-18 18:13:00 -0300

    Add TelemetryExtensions for OpenTelemetry integration
    Introduce TelemetryExtensions to configure OpenTelemetry for
    tracing and metrics in the WeatherAgentAPI. The extension
    method `AddTelemetry` sets up Azure Monitor Exporter to send
    telemetry data to Application Insights.
    
    - Configure `ResourceBuilder` with service metadata.
    - Add tracing with HTTP client instrumentation and custom
      source `WeaterAgentAPI`.
    - Add metrics with HTTP client instrumentation.
    - Use `APPLICATIONINSIGHTS_CONNECTION_STRING` for Azure
      Monitor Exporter configuration.

---
+++ src/WeatherAgent.API/Extensions/TelemetryExtensions.cs
@@ -0,0 +1,40 @@
+﻿using Azure.Monitor.OpenTelemetry.Exporter;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+namespace WeatherAgent.API.Extensions
+{
+    public static class TelemetryExtensions
+    {
+        public static IServiceCollection AddTelemetry(this IServiceCollection services, IConfigurationManager configuration)
+        {
+            var resourceBuilder = ResourceBuilder.CreateDefault()
+                .AddService(serviceName: "WeatherAgentAPI", serviceVersion: "1.0.0");
+
+            services.AddOpenTelemetry()
+                .WithTracing(builder =>
+                {
+                    builder.SetResourceBuilder(resourceBuilder)
+                    .AddSource("WeaterAgentAPI")
+                    .AddHttpClientInstrumentation()
+                    .AddAzureMonitorTraceExporter(options =>
+                    {
+                        options.ConnectionString = Environment.GetEnvironmentVariable("APPLICATIONINSIGHTS_CONNECTION_STRING");
+                    });
+                }).WithMetrics(builder =>
+                {
+                    builder.SetResourceBuilder(resourceBuilder)
+                        .AddHttpClientInstrumentation()
+                        .AddAzureMonitorMetricExporter(options =>
+                        {
+                            options.ConnectionString = Environment.GetEnvironmentVariable("APPLICATIONINSIGHTS_CONNECTION_STRING");
+                        });
+                });
+
+            return services;
+        }
+    }
+}
